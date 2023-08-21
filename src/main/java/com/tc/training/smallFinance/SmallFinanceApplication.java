f275bf0532a0f60795e48809a1ec43a5ff48c941
AnishTC <anish.marpalli@thoughtclan.com>
2023-08-21 11:52:28 +0530

    uploaded empty project

---
+++ src/main/java/com/tc/training/smallFinance/SmallFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.tc.training.smallFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tc/training/smallFinance/SmallFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.training.smallFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
